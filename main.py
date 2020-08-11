712d3da72d59dceb0d48b287bae94246a8945704
TacoBot <thomas.han@student.education.wa.edu.au>
2020-08-11 16:46:38 +0800

    new stuff, updated dankrate

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import time
 import os
 import random
 from discord.ext import commands
-from discord.ext.commands import has_permissions, CheckFailure
-from discord.ext.commands import Bot
+from discord.ext.commands import has_permissions, CheckFailure, Bot
 
 TOKEN = 'NTY2MTkzODI1ODc0MTgyMTY0.XLBbFw.o0yHAbU7R2yq5GnpdO7P7pzJyRY'
 PREFIX = (".", ">")
@@ -46,20 +45,35 @@ async def invite(ctx):
     await message_author.send('https://discord.com/api/oauth2/authorize?client_id=566193825874182164&permissions=8&scope=bot')
 
 
-@client.command()
-async def dankrate(ctx):
+@client.command(pass_context=True)
+async def dankrate(ctx, *, message):
     message_author = ctx.author
     message_channel = ctx.channel
-
-    aaaaa = random.randint(1, 101)
-    print("{} issued .dankrate 💸".format(message_author))
     
-    if aaaaa == 101:
-        embedVar = discord.Embed(
-            title="Dank r8 Machine", description=f"you broke the dank machine >:( :fire:", color=ff9933)
+    if len(message.content.split(' ')) > 1:
+        print(message)
+        
+        aaaaa = random.randint(1, 101)
+        print("{} issued .dankrate 💸".format(message_author))
+        
+        if aaaaa == 101:
+            embedVar = discord.Embed(
+                title="Dank r8 Machine", description=f"you broke the dank machine >:( :fire:\n{message} is {aaaaa}% dank", color=15105570)
+        else:
+            embedVar = discord.Embed(
+                title="Dank r8 Machine", description=f"{message} is {aaaaa}% dank", color=3066993)
+        await message_channel.send(embed=embedVar)
     else:
-        embedVar = discord.Embed(title="Dank r8 Machine", description=f"you are {aaaaa}% dank", color=ff9933)
-    await message_channel.send(embed=embedVar)
+        aaaaa = random.randint(1, 101)
+        print("{} issued .dankrate 💸".format(message_author))
+        
+        if aaaaa == 101:
+            embedVar = discord.Embed(
+                title="Dank r8 Machine", description=f"you broke the dank machine >:( :fire:", color=15105570)
+        else:
+            embedVar = discord.Embed(
+                title="Dank r8 Machine", description=f"you are {aaaaa}% dank", color=3066993)
+        await message_channel.send(embed=embedVar)
 
 
 client.run(TOKEN)
