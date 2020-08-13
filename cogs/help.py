5e3aa3c67b41ca2777f0eb5dc5d1e4b5be408087
NotTacoz <tacozpvp@gmail.com>
2020-08-13 11:57:32 +0800

    formatting help.pt

--- cogs/help.py
+++ cogs/help.py
@@ -11,27 +11,35 @@ async def help(self, ctx, *cog):
     try:
         if not cog:
             """Cog listing.  What more?"""
-            halp = discord.Embed(title='Cog Listing and Uncatergorized Commands',
-                                 description='Use `!help *cog*` to find out more about them!\n(BTW, the Cog Name Must Be in Title Case, Just Like this Sentence.)')
+            halp = discord.Embed(
+                title='Cog Listing and Uncatergorized Commands',
+                description='Use `!help *cog*` to find out more about them!\n(BTW, the Cog Name Must Be in Title Case, Just Like this Sentence.)'
+            )
             cogs_desc = ''
             for x in self.bot.cogs:
-                cogs_desc += ('{} - {}'.format(x,
-                                               self.bot.cogs[x].__doc__)+'\n')
+                cogs_desc += (
+                    '{} - {}'.format(x, self.bot.cogs[x].__doc__) + '\n')
             halp.add_field(
-                name='Cogs', value=cogs_desc[0:len(cogs_desc)-1], inline=False)
+                name='Cogs',
+                value=cogs_desc[0:len(cogs_desc) - 1],
+                inline=False)
             cmds_desc = ''
             for y in self.bot.walk_commands():
                 if not y.cog_name and not y.hidden:
-                    cmds_desc += ('{} - {}'.format(y.name, y.help)+'\n')
-            halp.add_field(name='Uncatergorized Commands',
-                           value=cmds_desc[0:len(cmds_desc)-1], inline=False)
+                    cmds_desc += ('{} - {}'.format(y.name, y.help) + '\n')
+            halp.add_field(
+                name='Uncatergorized Commands',
+                value=cmds_desc[0:len(cmds_desc) - 1],
+                inline=False)
             await ctx.message.add_reaction(emoji='✉')
             await ctx.message.author.send('', embed=halp)
         else:
             """Helps me remind you if you pass too many args."""
             if len(cog) > 1:
                 halp = discord.Embed(
-                    title='Error!', description='That is way too many cogs!', color=discord.Color.red())
+                    title='Error!',
+                    description='That is way too many cogs!',
+                    color=discord.Color.red())
                 await ctx.message.author.send('', embed=halp)
             else:
                 """Command listing within a cog."""
@@ -40,16 +48,21 @@ async def help(self, ctx, *cog):
                     for y in cog:
                         if x == y:
                             halp = discord.Embed(
-                                title=cog[0]+' Command Listing', description=self.bot.cogs[cog[0]].__doc__)
+                                title=cog[0] + ' Command Listing',
+                                description=self.bot.cogs[cog[0]].__doc__)
                             for c in self.bot.get_cog(y).get_commands():
                                 if not c.hidden:
                                     halp.add_field(
-                                        name=c.name, value=c.help, inline=False)
+                                        name=c.name,
+                                        value=c.help,
+                                        inline=False)
                             found = True
                 if not found:
                     """Reminds you if that cog doesn't exist."""
                     halp = discord.Embed(
-                        title='Error!', description='How do you even use "'+cog[0]+'"?', color=discord.Color.red())
+                        title='Error!',
+                        description='How do you even use "' + cog[0] + '"?',
+                        color=discord.Color.red())
                 else:
                     await ctx.message.add_reaction(emoji='✉')
                 await ctx.message.author.send('', embed=halp)
