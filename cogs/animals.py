f1d180888f7b134d0b2acb1f865c0be54b38dcb4
NotTacoz <tacozpvp@gmail.com>
2020-08-13 13:17:29 +0800

    e

--- cogs/animals.py
+++ cogs/animals.py
@@ -13,7 +13,7 @@ from discord.ext import commands
 from discord.ext.commands import has_permissions, CheckFailure, Bot
 from datetime import timedelta
 from io import BytesIO
-from utils import lists, default, argparser
+from utils import lists, permissions, http, default, argparser
 
 footer = "Made with ❤️ by Tacoz!"
 start_time = time.monotonic()
@@ -23,29 +23,34 @@ class Animals(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def randomimageapi(self, ctx, url, endpoint):
+        try:
+            r = await http.get(url, res_method="json", no_cache=True)
+        except aiohttp.ClientConnectorError:
+            return await ctx.send("The API seems to be down...")
+        except aiohttp.ContentTypeError:
+            return await ctx.send(
+                "The API returned an error or didn't return JSON...")
+
     @Bot.command()
-    @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
     async def cat(self, ctx):
         """ Posts a random cat """
         await self.randomimageapi(ctx, 'https://api.alexflipnote.dev/cats',
                                   'file')
 
     @Bot.command()
-    @Bot.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
     async def dog(self, ctx):
         """ Posts a random dog """
         await self.randomimageapi(ctx, 'https://api.alexflipnote.dev/dogs',
                                   'file')
 
     @Bot.command(aliases=["bird"])
-    @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
     async def birb(self, ctx):
         """ Posts a random birb """
         await self.randomimageapi(ctx, 'https://api.alexflipnote.dev/birb',
                                   'file')
 
     @Bot.command()
-    @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
     async def duck(self, ctx):
         """ Posts a random duck """
         await self.randomimageapi(ctx, 'https://random-d.uk/api/v1/random',
