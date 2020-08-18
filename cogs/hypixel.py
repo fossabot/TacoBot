2529e67b2bce8d96c3093ffb11fac5c8352ccf07
NotTacoz <tacozpvp@gmail.com>
2020-08-18 19:09:32 +0800

    bump

---
+++ cogs/hypixel.py
@@ -0,0 +1,29 @@
+import discord
+import os
+import sys
+import random
+import asyncio
+import time
+import hypixel
+import pypixel
+from random import choice
+from discord.ext import commands
+from discord.ext.commands import has_permissions, CheckFailure, Bot
+from datetime import timedelta
+
+footer = "Made with ❤️ by Tacoz!"
+start_time = time.monotonic()
+apikey = "INSERT API KEY"
+
+
+class Name(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['alias'])
+    async def commandname(self, ctx):
+        pass
+
+
+def setup(bot):
+    bot.add_cog(Name(bot))
