1a6cd0281ae7485b053400b90eb76c189b6aa304
NotTacoz <tacozpvp@gmail.com>
2020-08-22 13:53:13 +0800

    updater.bat autoupdate

--- cogs/image.py
+++ cogs/image.py
@@ -20,7 +20,35 @@ reddit = praw.Reddit(client_id="CFOX66IL6PXgRQ",
                      password="6x*JdQ@5h3t9")
 
 
-class Reddit(commands.Cog):
+def getMeme(self, subreddit, amount: int = None, time: str = None):
+    r = praw.Reddit(client_id=self.client_id,
+                    client_secret=self.client_secret,
+                    user_agent=self.user_agent)
+
+    subreddit = subreddit.replace("r/", "")
+
+    all_submissions = r.subreddit(subreddit)
+    posts = []
+
+    if not amount:
+        amount = 50
+    if not time:
+        time = 'month'
+
+    info = (f"Searching {subreddit} (Amount:{str(amount)})")
+
+    for submission in r.subreddit(subreddit).top(time, limit=amount):
+        if submission and not submission.stickied:
+            posts.append(submission)
+
+    post = posts[random.randint(1, amount) - 1]
+    while post.over_18:
+        warning = ("Post rated nsfw")
+        post = posts[random.randint(1, amount) - 1]
+    return {"title": post.title, "url": post.url, "upvotes": post.score}
+
+
+class Image(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -41,4 +69,4 @@ class Reddit(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(Reddit(bot))
+    bot.add_cog(Image(bot))
