f80997bd5ada540c4b9fb62f2cfcd6a2bdcc5c29
NotTacoz <tacozpvp@gmail.com>
2020-08-23 09:23:10 +0800

    info update

--- cogs/info.py
+++ cogs/info.py
@@ -34,6 +34,8 @@ class Info(commands.Cog):
                                  value=f"`{ctx.prefix}help utility`")
             help_embed.add_field(name="Images",
                                  value=f"`{ctx.prefix}help image`")
+            help_embed.add_field(name="Animals",
+                                 value=f"`{ctx.prefix}help animals`")
             help_embed.set_footer(text="use '.' before each command |" +
                                   footer)
         else:
