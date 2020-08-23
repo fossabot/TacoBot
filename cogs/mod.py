3010ef8bdbade1648feb32e24c4887d69911c0e6
NotTacoz <tacozpvp@gmail.com>
2020-08-23 15:01:21 +0800

    membedid

--- cogs/mod.py
+++ cogs/mod.py
@@ -12,7 +12,27 @@ from utils import permissions, default
 footer = "Made with ❤️ by Tacoz!"
 start_time = time.monotonic()
 
+class MemberID(commands.Converter):
+    async def convert(self, ctx, argument):
+        try:
+            m = await commands.MemberConverter().convert(ctx, argument)
+        except commands.BadArgument:
+            try:
+                return int(argument, base=10)
+            except ValueError:
+                raise commands.BadArgument(f"{argument} is not a valid member or member ID.") from None
+        else:
+            return m.id
 
+
+class ActionReason(commands.Converter):
+    async def convert(self, ctx, argument):
+        ret = argument
+
+        if len(ret) > 512:
+            reason_max = 512 - len(ret) - len(argument)
+            raise commands.BadArgument(f'reason is too long ({len(argument)}/{reason_max})')
+        return ret
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
