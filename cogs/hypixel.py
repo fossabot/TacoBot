48812feea66d9132b92f349fb1bd2aef1b5c0312
NotTacoz <tacozpvp@gmail.com>
2020-08-28 20:30:01 +0800

    hypixel update

--- cogs/hypixel.py
+++ cogs/hypixel.py
@@ -16,9 +16,18 @@ start_time = time.monotonic()
 apikey = "a54ce218-4fd5-4798-9b4b-6c74efac3456"
 
 
-def Arcade(username):
+def General(username):
     data = requests.get(
         f"https://api.hypixel.net/player?key={apikey}&name={username}").json()
+    if "rank" in data["player"] and data["player"]["rank"] != "NORMAL":
+        rank = data["player"]["rank"]
+    elif "newPackageRank" in data["player"]:
+        rank = data["player"]["newPackageRank"]
+    elif "packageRank" in data["player"]:
+        rank = data["player"]["packageRank"]
+    else:
+        rank = "Non"
+    name = data["player"]["displayname"]
 
 
 class Hypixel(commands.Cog):
@@ -26,7 +35,7 @@ class Hypixel(commands.Cog):
         self.bot = bot
 
     @commands.command(aliases=['alias'])
-    async def commandname(self, ctx):
+    async def commandname(self, ctx, *, message):
         pass
 
 
